c68140451a107a910989a65539ea60a05a1dba66
ナム Nam Nguyen <nhnam.develop@gmail.com>
2018-12-30 17:44:15 +0800

    Added utilities functions

--- index.js
+++ index.js
@@ -7,10 +7,14 @@ export default RNIphoneSafeArea;
 
 if (Platform.OS === "android") {
     module.exports = {
-        
+        hasNotch: function () {
+            return RNIphoneSafeArea.hasNotch();
+        }
     };
 } else if (Platform.OS === "ios") {
     module.exports = {
-        
+        hasNotch: function () {
+            return RNIphoneSafeArea.hasNotch();
+        }
     }
 }
--- ios/RNIphoneSafeArea.m
+++ ios/RNIphoneSafeArea.m
@@ -1,13 +1,46 @@
 
 #import "RNIphoneSafeArea.h"
-
+#import <UIKit/UIKit.h>
 @implementation RNIphoneSafeArea
 
 - (dispatch_queue_t)methodQueue
 {
     return dispatch_get_main_queue();
 }
+
 RCT_EXPORT_MODULE()
 
+- (BOOL) hasNotch {
+    UIViewController *root = [UIApplication sharedApplication].delegate.window.rootViewController;
+    UIEdgeInsets safeAreaInsets;
+    if (@available(iOS 11, *)) {
+        safeAreaInsets = root.view.safeAreaInsets;
+    } else {
+        safeAreaInsets = UIEdgeInsetsZero;
+    }
+    return safeAreaInsets.top > 0;
+}
+
+- (CGFloat) top {
+    UIViewController *root = [UIApplication sharedApplication].delegate.window.rootViewController;
+    UIEdgeInsets safeAreaInsets;
+    if (@available(iOS 11, *)) {
+        safeAreaInsets = root.view.safeAreaInsets;
+    } else {
+        safeAreaInsets = UIEdgeInsetsZero;
+    }
+    return safeAreaInsets.top;
+}
+
+- (CGFloat) bottom {
+    UIViewController *root = [UIApplication sharedApplication].delegate.window.rootViewController;
+    UIEdgeInsets safeAreaInsets;
+    if (@available(iOS 11, *)) {
+        safeAreaInsets = root.view.safeAreaInsets;
+    } else {
+        safeAreaInsets = UIEdgeInsetsZero;
+    }
+    return safeAreaInsets.bottom;
+}
 @end
   
